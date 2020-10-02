319eee82291b15ec8f15b1c4063f4bf688f6abaf
Ignacio Albornoz <ignacio.albornox@ug.uchile.cl>
2020-10-02 01:03:59 -0300

    Test commit.

--- src/main/java/com/github/cc3002/finalreality/gui/FinalReality.java
+++ src/main/java/com/github/cc3002/finalreality/gui/FinalReality.java
@@ -12,7 +12,7 @@ import javafx.stage.Stage;
  * <Complete here with the details of the implemented application>
  *
  * @author Ignacio Slater Muñoz.
- * @author <Your name>
+ * @author <Ignacio Albornoz A.>
  */
 public class FinalReality extends Application {
 
